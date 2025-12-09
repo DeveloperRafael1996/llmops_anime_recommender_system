57c4323035ab71b992c43111beec0e0400841e3c
Rafael Guevara Aller <djzm80@gmail.com>
2025-12-09 18:18:41 -0500

    chore: add log into terminal

--- utils/logger.py
+++ utils/logger.py
@@ -1,19 +1,31 @@
 import logging
 import os
+import sys
 from datetime import datetime
 
 LOGS_DIR = "logs"
-os.makedirs(LOGS_DIR,exist_ok=True)
+os.makedirs(LOGS_DIR, exist_ok=True)
 
 LOG_FILE = os.path.join(LOGS_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
-
-logging.basicConfig(
-    filename=LOG_FILE,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 def get_logger(name):
     logger = logging.getLogger(name)
+    
+    if logger.handlers:
+        return logger
+    
     logger.setLevel(logging.INFO)
+    
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
     return logger
